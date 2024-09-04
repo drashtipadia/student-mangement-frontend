3b0f3052928389f6f5d7752a4e89776da0435879
drashtipadia <djpadia03@gmail.com>
2024-09-04 14:19:31 +0530

    React init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { admissionform } from './Components/admissionform';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+            <h1>Hello</h1>
+          
+      </div>
+      <admissionform/>
+      </>
+  );
+}
+
+export default App;
---
+++ src/Components/admissionform.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const admissionform = () => {
+  return (
+    <div>
+        <h1>Admission Form</h1>
+        <form>
+          <label>Email:</label>
+          <input type='email' name='email' />
+        </form>
+    </div>
+  )
+}
