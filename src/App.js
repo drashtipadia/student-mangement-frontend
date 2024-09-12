d110364a48479fa0da51b7bf766f213a9e25b4df
raj-p26 <rajpeshavariya26@gmail.com>
2024-09-12 20:00:04 +0530

    Minor Bug Fixes :)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 import { Header } from "./Component/Header";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 function App() {
@@ -12,13 +10,13 @@ function App() {
       <div className="conatiner p-5 bg-dark">
         <div className="row">
           <div className="col">
-            <Link to={"admissionForm"}>
+            <Link to={"/admissionForm"}>
               <button className="btn btn-primary">AdmissionForm</button>
             </Link>
           </div>
 
           <div className="col">
-            <Link to={"viewdata"}>
+            <Link to={"/viewdata"}>
               <button className="btn btn-primary">View Data</button>
             </Link>
           </div>
@@ -26,10 +24,6 @@ function App() {
           <div className="col">
             <button className="btn btn-primary">Bonafied Certificate</button>
           </div>
-
-          {/* <li>
-                  <FontAwesomeIcon icon={faPhone} />{" "}
-                </li> */}
         </div>
       </div>
     </div>
---
+++ src/views/ViewStudent.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const SERVER_HOST = process.env.SERVER_HOST || "localhost";
+const SERVER_PORT = Number(process.env.SERVER_PORT) || 8000;
+
+/**
+ * returns object containing student information.
+ * @param {string} id ID of the student
+ */
+const fetchStudent = async (id) => {
+  const response = await fetch(
+    `http://${SERVER_HOST}:${SERVER_PORT}/students/${id}`
+  );
+  return await response.json();
+};
+
+export function ViewStudent() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetchStudent(params.id).then((stud) => {
+      student = stud;
+      setIsLoading(false);
+      console.log(student);
+    });
+  }, []);
+  let student = {};
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Student {params.id}</h1>
+      <hr />
+      {isLoading ? <h2>Loading...</h2> : <h3>Student Loaded</h3>}
+    </>
+  );
+}
